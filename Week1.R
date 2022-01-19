e0d89d8311459173422eafff88072321b2d26219
GOseiMainoo <89168440+GOseiMainoo@users.noreply.github.com>
2022-01-18 19:48:53 -0500

    Add files via upload

---
+++ Week1.R
@@ -0,0 +1,32 @@
+# This line is showing Boolean function for 'And'
+2 & 2
+# This line shows the 'if' function above is TRUE
+if (2&2) print("2&2 is true")
+
+#Second example of Boolean function "And'
+2 & 3
+# This line shows the 'if' function above is TRUE
+if (2&3) print("2&3 is true")
+
+
+# This line is showing Boolean function 'or'
+2 | 3
+
+#This line shows the 'if' function of 'or'
+if (2|3) print ("2|3 is true")
+
+# Second example showing Boolean function 'or'
+1 | 4
+#This line shows the 'if' function of 'or'
+if (1|4) print ("1|4 is true")
+
+
+#This line explains the  Boolean function 'not'
+!2
+#This line is explaining the 'if' function 'not'
+if (!2)print ("!2 is false" )
+
+#another example of Boolean function 'not'
+!6
+#This line is showing the 'if' function 'not'
+if (!6)print ("!6 is false")
